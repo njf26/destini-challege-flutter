bb7e5590063d4539139c55b20a87c1ceffd8a6a5
njf26 <farnsworth.natalie@gmail.com>
2020-09-19 15:49:35 -0600

    Uploaded Destini Challenge flutter app

---
+++ lib/story.dart
@@ -0,0 +1,17 @@
+class Story {
+  String _storyTitle;
+  String _choice1;
+  String _choice2;
+
+  Story({String storyTitle, String choice1, String choice2}) {
+    _storyTitle = storyTitle;
+    _choice1 = choice1;
+    _choice2 = choice2;
+  }
+
+  String get choice2 => _choice2;
+
+  String get choice1 => _choice1;
+
+  String get storyTitle => _storyTitle;
+}
